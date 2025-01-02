3fe5d3d2116ae00f7004b96defa7db69a240dd89
부수환 <rndjdieo119@naver.com>
2025-01-02 14:24:57 +0900

    feat : 404

--- deploy-react/src/App.tsx
+++ deploy-react/src/App.tsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import BaseScreen from "./feature/screen/BaseScreen";
 import Home from "./feature/home/Home";
+import ErrorPage from "./feature/error/ErrorPage";
 
 const provider = createBrowserRouter([
   {
     path: "/",
     element: <BaseScreen />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
